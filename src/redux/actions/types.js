3fad85e56a570b03559a355bd0022bbbb39da051
diganto <diganta.datta.3@gmail.com>
2021-04-03 20:22:06 +0530

    Another one

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -13,4 +13,4 @@ export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 
 
-export const BACKEND_URL = "https://7aj8mmam0g.execute-api.ap-south-1.amazonaws.com/dev";
+export const BACKEND_URL = "https://vbs6l9lgla.execute-api.ap-south-1.amazonaws.com/dev";
