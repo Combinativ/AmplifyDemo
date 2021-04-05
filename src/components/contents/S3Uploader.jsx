f2983658b164ce4c0dc5cbf52003f60e428a102b
ritarajpal32 <ritarajpal32@gmail.com>
2021-04-05 21:56:57 +0530

    uploader testing wip

--- src/components/contents/S3Uploader.jsx
+++ src/components/contents/S3Uploader.jsx
@@ -52,19 +52,19 @@ const S3Uploader = () => {
         signingUrl={url}
         signingUrlMethod="POST"
         // accept="image/*"
-        s3path="/uploads/"
+        //s3path="/uploads/"
         // preprocess={onUploadStart}
         // onSignedUrl={onSignedUrl}
         // onProgress={onUploadProgress}
         // onError={onUploadError}
         // onFinish={onUploadFinish}
-        // signingUrlHeaders={{ additional: headers }}
-        // signingUrlQueryParams={{ additional: query-params }}
-        signingUrlWithCredentials={ true }      // in case when need to pass authentication credentials via CORS
-        uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }}  // this is the default
-        contentDisposition="auto"
+        //signingUrlHeaders={{ additional: headers }}
+        //signingUrlQueryParams={{ additional: query-params }}
+        //signingUrlWithCredentials={ true }      // in case when need to pass authentication credentials via CORS
+        //uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }}  // this is the default
+        //contentDisposition="auto"
         // inputRef={cmp => this.uploadInput = cmp}
-        autoUpload={true}
+        //autoUpload={true}
       />
       {/* <ReactS3Uploader 
         signingUrl="/s3/sign"
