74f573fc58dc1f439165068f69cf7c93d8a2dc51
ritarajpal32 <ritarajpal32@gmail.com>
2021-04-06 14:26:03 +0530

    trying url 2

--- src/components/contents/S3Uploader.jsx
+++ src/components/contents/S3Uploader.jsx
@@ -51,7 +51,7 @@ const S3Uploader = () => {
       contentType: file.type
     };
    
-    axios.get(url1, { params })
+    axios.get(url2, { params })
     .then(data => {
       callback(data);
     })
